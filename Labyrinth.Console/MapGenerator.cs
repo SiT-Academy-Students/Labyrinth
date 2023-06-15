346f6e042794e17414eee4032234a6c2dcfaba36
Tony Troeff <tonyslav_troeff@outlook.com>
2023-06-15 20:25:13 +0300

    Added a method to normalize the edges between neighbouring obstacles.

--- Labyrinth.Console/MapGenerator.cs
+++ Labyrinth.Console/MapGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using Labyrinth.Console.Controllers;
 using Labyrinth.Console.Obstacles;
 using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
 using System.Security.Cryptography.X509Certificates;
 
 namespace Labyrinth.Console
@@ -30,36 +31,15 @@ namespace Labyrinth.Console
             Obstacle obstacle = new Obstacle(coordinates, edges);
             this._obstaclesDict[obstacle.Coordinates] = obstacle;
             this._flowController.RenderObstacle(obstacle);
-        }
 
-        public void GenerateXAxisBorders(int cycleStart, int cycleEnd, ObstacleEdges edgeOrientation)
-        {
-            for(int startX = cycleStart; startX < cycleEnd; startX++)
-            {
-                Coordinates top = new Coordinates { X = startX, Y = this._topBorder };
-                RenderObstacle(top, edgeOrientation);
-                
-                Coordinates bot = new Coordinates { X = startX, Y = this._botBorder - 1 };
-                RenderObstacle(bot, edgeOrientation);
-            }
-        }
-        public void GenerateYAxisBorders(int cycleStart, int cycleEnd, ObstacleEdges edgeOrientation)
-        {
-            for (int startY = cycleStart; startY < cycleEnd; startY++)
-            {
-                Coordinates left = new Coordinates { X = this._leftBorder, Y = startY };
-                RenderObstacle(left, edgeOrientation);
-
-                Coordinates right = new Coordinates { X = this._rightBorder - 1, Y = startY };
-                RenderObstacle(right, edgeOrientation);
-            }
+            this.NormalizeEdges(coordinates);
         }
 
         public void GenerateMapBorders()
         {
             ObstacleEdges edgesOrientX = ObstacleEdges.Right | ObstacleEdges.Left;
             GenerateXAxisBorders(this._leftBorder, this._rightBorder, edgesOrientX);
-            
+
             ObstacleEdges edgesOrientY = ObstacleEdges.Top | ObstacleEdges.Bottom;
             GenerateYAxisBorders(this._topBorder, this._botBorder, edgesOrientY);
         }
@@ -74,6 +54,51 @@ namespace Labyrinth.Console
 
                 Coordinates currentObstacleCoordinates = new Coordinates { X = randomObstacleX, Y = randomObstacleY };
                 RenderObstacle(currentObstacleCoordinates, randomObstacleEdges);
+            }
+        }
+
+        private void NormalizeEdges(Coordinates coordinates)
+        {
+            this.NormalizeEdges(coordinates, coordinates with { Y = coordinates.Y - 1 }, ObstacleEdges.Top, ObstacleEdges.Bottom);
+            this.NormalizeEdges(coordinates, coordinates with { X = coordinates.X + 1 }, ObstacleEdges.Right, ObstacleEdges.Left);
+            this.NormalizeEdges(coordinates, coordinates with { Y = coordinates.Y + 1 }, ObstacleEdges.Bottom, ObstacleEdges.Top);
+            this.NormalizeEdges(coordinates, coordinates with { X = coordinates.X - 1 }, ObstacleEdges.Left, ObstacleEdges.Right);
+        }
+
+        private void NormalizeEdges(Coordinates source, Coordinates destination, ObstacleEdges sourceEdges, ObstacleEdges destinationEdges)
+        {
+            if (!this._obstaclesDict.ContainsKey(destination)) return;
+
+            Obstacle sourceObstacle = this._obstaclesDict[source];
+            if ((sourceObstacle.Edges & sourceEdges) == ObstacleEdges.None)
+                this.RenderObstacle(sourceObstacle.Coordinates, sourceObstacle.Edges | sourceEdges);
+
+            Obstacle destinationObstacle = this._obstaclesDict[destination];
+            if ((destinationObstacle.Edges & destinationEdges) == ObstacleEdges.None)
+                this.RenderObstacle(destinationObstacle.Coordinates, destinationObstacle.Edges | destinationEdges);
+        }
+
+        private void GenerateXAxisBorders(int cycleStart, int cycleEnd, ObstacleEdges edgeOrientation)
+        {
+            for (int startX = cycleStart; startX < cycleEnd; startX++)
+            {
+                Coordinates top = new Coordinates { X = startX, Y = this._topBorder };
+                RenderObstacle(top, edgeOrientation);
+
+                Coordinates bot = new Coordinates { X = startX, Y = this._botBorder - 1 };
+                RenderObstacle(bot, edgeOrientation);
+            }
+        }
+
+        private void GenerateYAxisBorders(int cycleStart, int cycleEnd, ObstacleEdges edgeOrientation)
+        {
+            for (int startY = cycleStart; startY < cycleEnd; startY++)
+            {
+                Coordinates left = new Coordinates { X = this._leftBorder, Y = startY };
+                RenderObstacle(left, edgeOrientation);
+
+                Coordinates right = new Coordinates { X = this._rightBorder - 1, Y = startY };
+                RenderObstacle(right, edgeOrientation);
             }
         }
     }
